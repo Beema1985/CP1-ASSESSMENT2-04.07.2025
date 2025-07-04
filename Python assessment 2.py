27d49f94dc10e28f8f3ee95cc934a0f7aede4d89
Jeyaditya <jeyaditya@jainvidyalaya.in>
2025-07-04 15:10:01 +0530

    Jeyaditya A

---
+++ Python assessment 2.py
@@ -0,0 +1,37 @@
+##Python assessment 2 by Jeyaditya
+##Created on 4/7/2025
+
+
+
+##print('This is the finder of multiples of 11 you have given the range for:')
+##start=int(input('Enter the start value:   '))
+##end=int(input('Enter the end value:   '))
+##
+##for i in range(start,end):
+##   a= i%11
+##   if a==0:
+##        print(i)
+##   else:()
+
+
+
+##Python assessment 3 by Jeyaditya
+##Created on 4/7/2025
+##for i in range(1,41,3):
+##  print( i, end='  ')
+
+##Extra program.
+for i in range(1,41,3):
+    print(i, end='  ')
+
+        
+
+
+
+
+
+
+
+
+
+    
