7eaa56aa5e10d9e89bd8bfb09f7d2fa1bde5b498
sri133 <sriprasath@jainvidyalaya.in>
2025-07-05 09:40:10 +0530

    Sri prasatth.P

---
+++ ass 2 ai b.py
@@ -0,0 +1,25 @@
+#assessment 2_04-07-2025
+
+##start = int(input("Enter the starting value: "))
+##end = int(input("Enter the ending value: "))
+##print("Numbers divisible by 11 between {start} and {end}:")
+##for number in range(start, end + 1):
+##    if number % 11 == 0:
+##        print(number)
+
+#assessment3_04-07-2025
+
+##for i in range(1, 41, 3):
+##    print(i, end=', ')
+
+##i = 1
+##while True:
+##    term = (-1) ** (i + 1) * (3 * i - 2)
+##    print(term, end=", " if term != -40 else "")
+##    if term == -40:
+##        break
+##    i += 1
+
+
+
+
