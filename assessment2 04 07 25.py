331bd004503310f6614c9afc29bb399a443fade0
saif2610 <mohammed@jainvidyalaya.in>
2025-07-05 09:29:32 +0530

    Add files via upload

---
+++ assessment2 04 07 25.py
@@ -0,0 +1,18 @@
+##start = int(input("Enter the starting value: "))
+##end = int(input("Enter the ending value: "))
+##print('number divisible by 11')
+##for number in range(start, end + 1):
+##    if number % 11 == 0:
+##        print(number)
+      
+      
+##for i in range(1, 41, 3):
+##    print(i, end=',')
+
+
+
+
+
+
+
+
