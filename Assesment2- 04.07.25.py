f316a98cf5cdfd1966482ab69726798ac3c22b31
Raksmi~ <raksmi@jainvidyalaya.in>
2025-07-06 15:15:50 +0530

    Add files via upload

---
+++ Assesment2- 04.07.25.py
@@ -0,0 +1,25 @@
+#Assesment 2- 04.07.25
+#1.write a program that gets 2 inputs from the user, starting and the ending value and print the number tht starts with 11
+
+##a=int(input('Enter the 1st num:'))
+##b=int(input('Enter the 2nd num:'))
+##for i in range(a,b):
+##    if i%11==0:
+##        print(i)
+
+
+#Assesment 3-04.07.25
+
+##a=1
+##b=3
+##for i in range(1,41,3):
+##    print(i,end=" ")
+
+for i in range(1,41,3):
+    print(i*-1+3)
+   
+   
+    
+    
+    
+        
